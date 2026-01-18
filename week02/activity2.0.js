e2f840824b6eebbb4f50cf26b9db77be8c8a1fea
moisesph <mpleytez@byupathway.edu>
2026-01-18 14:25:19 -0600

    fdasfd

---
+++ week02/activity2.0.js
@@ -0,0 +1,28 @@
+const DAYS = 6;
+const LIMIT = 30;
+let studentReport = [11, 42, 33, 64, 29, 37, 44];
+
+
+
+for (let i = 0; i < studentReport.length; i++) {
+    if (student < LIMIT) {
+        console.log(`${student}`);
+    }
+}
+
+studentReport.forEach((student) => {
+    if (student < LIMIT) {
+        console.log(`${student}`);
+    }
+});
+
+
+while (student < LIMIT) {
+    console.log(`${student}`);
+};
+
+for (let i in studentReport) {
+    if (studentReport[i] < LIMIT) {
+        console.log(studentReport[i]);
+    }
+};
---
+++ week02/projectbomexercice/scripts/script.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+const li = document.createElement('li');
+li.textContent = input.value;
+const deleteButton = document.createElement('button');
+deleteButton.textContent = '❌';
+li.append(deleteButton);
+list.append(li);
