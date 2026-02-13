dacf024d6cd4ab106f9ad4912d7a7bfbb65babda
moisesph <mpleytez@byupathway.edu>
2026-02-13 09:58:10 -0600

    adfdfa

--- scripts/review.js
+++ scripts/review.js
@@ -5,7 +5,7 @@ let visitsNum = Number(window.localStorage.getItem("reviews-counts")) || 0;
 if (visitsNum !== 0) {
     displayVisit.textContent = visitsNum;
 } else {
-    displayVisit.textContent = `1, Welcome, this is your first review!!`
+    displayVisit.textContent = `Welcome, this is your first review!!`
 }
 
 visitsNum++;
